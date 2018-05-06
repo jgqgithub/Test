5a2985604747db8dbae3736ee3021d511dc40f71
jgqgithub <jiguoqiang@acctrue.com>
2018-05-06 21:58:42 +0800

    '初始项目'

---
+++ WebApplication5/Startup1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebApplication5.Startup1))]
+
+namespace WebApplication5
+{
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
---
+++ WebApplication5/TestHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace WebApplication5
+{
+    public class TestHub : Hub
+    {
+        public void Hello(string name)
+        {
+            Clients.All.hello(name+"你好");
+        }
+    }
+}
