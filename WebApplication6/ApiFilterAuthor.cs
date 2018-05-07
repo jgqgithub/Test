cb58f5e30528ed90f3b16859f478a309d0f6665e
jgqgithub <jiguoqiang.acctrue.com>
2018-05-07 14:06:29 +0800

    'WebApiTest'

---
+++ WebApplication6/ApiFilterAuthor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace WebApplication6
+{
+    public class ApiFilterAuthor : IAuthorizationFilter
+    {
+        public bool AllowMultiple
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public async Task<HttpResponseMessage> ExecuteAuthorizationFilterAsync(HttpActionContext actionContext, CancellationToken cancellationToken, Func<Task<HttpResponseMessage>> continuation)
+        {
+            IEnumerable<string> usernames;
+            if (!actionContext.Request.Headers.TryGetValues("username", out usernames))
+            {
+                return  new HttpResponseMessage(HttpStatusCode.Unauthorized) { Content = new StringContent("报文头中的UserName为空") };
+            }
+            IEnumerable<string> passwords;
+            if (!actionContext.Request.Headers.TryGetValues("password", out passwords))
+            {
+                return new HttpResponseMessage(HttpStatusCode.Unauthorized) { Content = new StringContent("报问头中的password为空") };
+            }
+            string username = usernames.First();
+            string password = passwords.First();
+            if (username == "admin" && password == "123456")
+            {
+                return await continuation();
+            }
+            else
+            {
+                return new HttpResponseMessage(HttpStatusCode.Unauthorized) { Content = new StringContent("验证失败") };
+            }
+        }
+    }
+}
---
+++ WebApplication6/Controllers/PersonController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication6.Models;
+
+namespace WebApplication6.Controllers
+{
+    [RoutePrefix("api/Person")]
+    public class PersonController : ApiController
+    {
+        List<Person> lists = new List<Person>();
+        [HttpGet]
+        [Route("GetPerson")]
+        public IEnumerable<Person> GetPerson()
+        {
+          
+            lists.Add(new Person() { Name = "xiaoming", Id = 2 });
+            lists.Add(new Person() { Name = "xiaohong", Id = 3 });
+            return lists;
+        }
+        [Route("GetPerson")]
+        public Person GetPerson(int id)
+        {
+            return lists.Where(i => i.Id == id).SingleOrDefault();
+        }
+        [HttpPost]
+        [Route("AddPerson")]
+        public bool AddPerson([FromBody]Person p)
+        {
+            lists.Add(p);
+            return lists.Count >=1;
+        }
+
+    }
+}
